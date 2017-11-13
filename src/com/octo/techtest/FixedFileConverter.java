5b0c8622c4fc51428070ba084501f8fde4fcc5a2
nick.yiming.gao <nick.yiming.gao@gmail.com>
2017-11-14 01:42:15 +1100

    Clean up unused imports.

--- src/com/octo/techtest/FixedFileConverter.java
+++ src/com/octo/techtest/FixedFileConverter.java
@@ -3,15 +3,10 @@ package com.octo.techtest;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
