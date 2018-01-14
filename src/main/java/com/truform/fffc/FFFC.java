a57b8e7039c360eec299b484798f836302b0633b
Henry Barnett <hbarnett@alassian.com>
2018-01-14 20:37:17 +1100

    Removing redundant comment and import

--- src/main/java/com/truform/fffc/FFFC.java
+++ src/main/java/com/truform/fffc/FFFC.java
@@ -6,15 +6,11 @@ import com.truform.fffc.helpers.MetadataReader;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-/**
- * Hello world!
- */
 public class FFFC {
     public static void main(String[] args) {
         if (args.length != 3) {
