8e50c5e7ce5baa92e6862fd10c19920eb5ca115c
Henry Barnett <hbarnett@alassian.com>
2018-01-14 12:41:06 +1100

    Initial commit for maven setup

---
+++ fffc-core/src/test/java/com/truform/fffc/AppTest.java
@@ -0,0 +1,19 @@
+package com.truform.fffc;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for FFFC.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertThat(true, is(true));
+    }
+}
