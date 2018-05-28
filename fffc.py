d5e331f396ce721e2e5153be375c9e2c5f306f75
Rizwan Ahamed <rizwan.@rizwan.in>
2018-05-29 07:26:09 +1000

    fix(fffc): generated csv was storing indices
    the dataframe was storing the indices by default - setting it to false

--- fffc.py
+++ fffc.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('fffc_data.txt', sep=" ", header=None, names=["Birth date", "First name", "Last name", "Weight"])
-dataset.to_csv('fffc_converted.csv', encoding='utf-8')
+dataset.to_csv('fffc_converted.csv', encoding='utf-8', index=False)
