1914085137f3a762ba6441ff5411d35aeb32fb11
Rizwan Ahamed <rizwan.@rizwan.in>
2018-05-29 10:50:21 +1000

    feat(fffc): added columns with meta-data , formatted dates
    loaded metadata from another file for columns - formatted the date - wrote the output file
    
    BREAKING CHANGE: columns and date formatting

--- fffc.py
+++ fffc.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('fffc_data.txt', sep=" ", header=None, names=["Birth date", "First name", "Last name", "Weight"])
+# load meta data for columns
+metadata = pd.read_csv('fffc_metadata.txt', sep=",", header=None)
+columns = metadata.iloc[:, 0].values
+print(columns)
+
+# load dataset
+dataset = pd.read_csv('fffc_data.txt', sep=" ", header=None, names=columns)
+# convert column 0 which is date to datetime for formatting
+dataset[columns[0]] = pd.to_datetime(dataset[columns[0]])
+#format date
+dataset[columns[0]] = dataset[columns[0]].dt.strftime("%d/%m/%Y")
+print(dataset)
+
+# write the dataset to CSV
 dataset.to_csv('fffc_converted.csv', encoding='utf-8', index=False)
