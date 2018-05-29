56df0d6e8184fb88f616f215f8c0b6cb007388d3
Rizwan Ahamed <rizwan.@rizwan.in>
2018-05-29 11:28:59 +1000

    feat(fffc): added validation
    Added validation for Weight using pandas validator for dataframe - numeric validation - exit if
    fails use pip install pandas_validator
    
    BREAKING CHANGE: Validation

--- fffc.py
+++ fffc.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import pandas_validator as pv
+import sys
+
+class DataSetValidator(pv.DataFrameValidator):
+    weight = pv.FloatColumnValidator('Weight', min_value=0, max_value=200)
+
+validator = DataSetValidator()
 
 # load meta data for columns
 metadata = pd.read_csv('fffc_metadata.txt', sep=",", header=None)
@@ -7,10 +14,21 @@ print(columns)
 
 # load dataset
 dataset = pd.read_csv('fffc_data.txt', sep=" ", header=None, names=columns)
-# convert column 0 which is date to datetime for formatting
+
+print(validator.is_valid(dataset))
+if validator.is_valid(dataset) == False:
+    print("Validation Failed")
+    sys.exit(1)
+
+
+# convert Birth date - column 0 to datetime for formatting
 dataset[columns[0]] = pd.to_datetime(dataset[columns[0]])
 #format date
 dataset[columns[0]] = dataset[columns[0]].dt.strftime("%d/%m/%Y")
+
+# format trailing spaces for first and last names
+dataset[columns[1]] = dataset[columns[1]].map(lambda x: x.strip())
+dataset[columns[2]] = dataset[columns[2]].map(lambda x: x.strip())
 print(dataset)
 
 # write the dataset to CSV
