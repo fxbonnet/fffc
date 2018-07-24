14d18551d570266c76ca6f5a66cef6c841d54606
amolksagar <kksagar.amol@gmail.com>
2018-07-24 16:47:10 +1000

    Added validations for the data fields

--- src/main/java/com/octo/au/domain/validation/FieldValidator.java
+++ src/main/java/com/octo/au/domain/validation/FieldValidator.java
@@ -12,18 +12,45 @@ public class FieldValidator implements DataValidator {
     @Override
     public void validate(List<DataRow> dataRows,Structure metadataStructure) throws CustomException {
         StringBuilder errorFields = new StringBuilder();
+        int rowNumber=1;
         for(DataRow dataRow: dataRows){
         	for(DataColumn dataColumn : dataRow.getColumns()){
+        		int columnNumber=1;
         		if(dataColumn.getType().equalsIgnoreCase(Constants.STR)){
-        		errorFields.append(ValidatorUtil.notNullString.and(ValidatorUtil.notEmptyString)
-                        .and(ValidatorUtil.stringBetween(1, metadataStructure.getCt().get(dataColumn.getColumnIndex()).getLength())).test((String)dataColumn.getValue())
-                        .getFieldNameIfInvalid(" Please specify valid data for  "+metadataStructure.getCt().get(dataColumn.getColumnIndex()).getName()).orElse(""));
+        		    errorFields.append(ValidatorUtil.notNullString
+        			.and(ValidatorUtil.notEmptyString)
+                    .and(ValidatorUtil.stringBetween(1, metadataStructure.getCt().get(dataColumn.getColumnIndex()).getLength()))
+                    .test(dataColumn.getValue().toString())
+                    .getFieldNameIfInvalid(" Please specify valid data for "+metadataStructure.getCt().get(dataColumn.getColumnIndex()).getName()
+                        						+ " with value "+ dataColumn.getValue().toString()
+                        						+ " on row number "+rowNumber
+                        						+ " and column number "+columnNumber).orElse(""));
         		}
+        		else if(dataColumn.getType().equalsIgnoreCase(Constants.STR_NUMERIC)){
+        			 errorFields.append(ValidatorUtil.notNullString
+        	        .and(ValidatorUtil.notEmptyString)
+        	        .and(ValidatorUtil.stringBetween(1, metadataStructure.getCt().get(dataColumn.getColumnIndex()).getLength()))
+                    .and(ValidatorUtil.isValidinteger())
+                    .test(dataColumn.getValue().toString())
+                    .getFieldNameIfInvalid(" Please specify valid data for "+metadataStructure.getCt().get(dataColumn.getColumnIndex()).getName()
+                        						+ " with value "+ dataColumn.getValue().toString()
+                        						+ " on row number "+rowNumber
+                        						+ " and column number "+columnNumber).orElse(""));
+        		}
+        		/*else if(dataColumn.getType().equalsIgnoreCase(Constants.STR_DATE)){
+        			errorFields.append(ValidatorUtil.notNullString
+            				.and(ValidatorUtil.notEmptyString)
+                            .and(ValidatorUtil.stringBetween(1, metadataStructure.getCt().get(dataColumn.getColumnIndex()).getLength()))
+                            .test(dataColumn.getValue().toString())
+                            .getFieldNameIfInvalid(" Please specify valid data for "+metadataStructure.getCt().get(dataColumn.getColumnIndex()).getName()
+                            						+ " with value "+ dataColumn.getValue().toString()
+                            						+ " on row number "+rowNumber
+                            						+ " and column number "+columnNumber).orElse(""));
+        		}*/
+        		columnNumber++;
         	}
+        	rowNumber++;
         }
-        /*errorFields.append(ValidatorUtil.notNullInteger.and(ValidatorUtil.greaterThanZero)
-            .and(ValidatorUtil.integerBetween(18, 60)).test(employee.getAge())
-            .getFieldNameIfInvalid(" Please specify valid age ").orElse(""));*/
         String errors = errorFields.toString();
         if (!errors.isEmpty()) {
             throw new CustomException(errors);
