45a7a9368209468762859b395dafe83c9728b1b0
brian-hyperanna <brian@hyperanna.com>
2018-10-16 21:21:36 +1100

    move to the right locations

---
+++ solutions/main.py
@@ -0,0 +1,10 @@
+from FixedFileFormatConverter import FixedFileFormatConverter
+
+
+def main():
+    fixed_file_format_converter = FixedFileFormatConverter()
+    fixed_file_format_converter.load_metadata("metadata")
+
+
+if __name__ == "__main__":
+    main()
