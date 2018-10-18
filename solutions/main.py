bb84ff20184c59ea188d82dcc105a049f7a1e2d5
brian-hyperanna <brian@hyperanna.com>
2018-10-18 11:35:24 +1100

    add arguments for command line

--- solutions/main.py
+++ solutions/main.py
@@ -2,15 +2,39 @@
 from FixedFileFormatConverter import FixedFileFormatConverter
 import logging
 import os
+import argparse
 
 LOG = logging.getLogger()
 LOG.addHandler(logging.StreamHandler())
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Fixed Format File Converter')
+
+    parser.add_argument('--metadata',
+                        required=True,
+                        help='The metadata file describing the structure')
+
+    parser.add_argument('--data',
+                        required=True,
+                        help='The fixed format raw data file')
+
+    parser.add_argument('--output',
+                        default='output',
+                        required=False,
+                        help='The output file name prefix')
+
+    return parser.parse_args()
+
+
 def main():
-    fixed_file_format_converter = FixedFileFormatConverter()
-    fixed_file_format_converter.load_metadata("metadata")
-    fixed_file_format_converter.load_raw_data("data")
+    args = get_args()
+    metadata = args.metadata
+    data = args.data
+    output = args.output
+    fixed_file_format_converter = FixedFileFormatConverter(metadata=metadata, raw_data=data, output=output)
+    fixed_file_format_converter.load_metadata()
+    fixed_file_format_converter.load_raw_data()
 
 
 if __name__ == "__main__":
