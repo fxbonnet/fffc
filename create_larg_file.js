7552ed9dc8d4ca052e4c1afe238f908e77c256e3
Alireza Bayat <www.alireza4@gmail.com>
2018-11-11 17:08:59 +1100

    - Improved error handling

--- create_larg_file.js
+++ create_larg_file.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 const file = fs.createWriteStream("./big.file");
 
-//To create a big file for the test purpouse
+// To create a big file for the test purpose
+// this will create a big file
+// Excel is not comfortable with opening csv file over a millions row
 for (let i = 0; i <= 1e6; i++) {
   file.write(
-    "1970-01-01John           Smith     ^^^  ??    81.5contract  1989-06-11***&$\n"
+    "1970-01-01John           Sm,ith     ^^^  ??   81.5contract  1989-06-11***&$\n"
+  );
+
+  file.write(
+    "1975-01-31Jane           Doe,       %%%  **   61.1perman@@ent 1993-12-24\n"
   );
   file.write(
-    "1975-01-31Jane           Doe       %%%  **    61.1perman@@ent 1993-12-24\n"
-  );
-  file.write(
-    "1988-11-28Bob    ??        Big    !!!!        102.4permanent 2000-09-17 ?!\n"
+    "1988-11-28,Bob    ??       Big    !!!!        102.4permanent 2000-09-17 ?!\n"
   );
 }
 
--- readFile.js
+++ readFile.js
@@ -9,7 +9,10 @@ const readFile = util.promisify(fs.readFile);
  */
 function readLine(filePath) {
   const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
+    input: fs.createReadStream(filePath).on("error", () => {
+      console.log(new Error(`Make sure ${filePath} exist`));
+      return;
+    }),
     crlfDelay: Infinity
   });
   return rl;
@@ -17,10 +20,10 @@ function readLine(filePath) {
 
 /**
  * load a file asynchronously
- * @param {String} metaDataFilePath
+ * @param {String} metadataFilePath
  */
-async function dataColumns(metaDataFilePath) {
-  return await readFile(metaDataFilePath);
+async function dataColumns(metadataFilePath) {
+  return await readFile(metadataFilePath);
 }
 
 /**
@@ -38,12 +41,23 @@ function stringSanitizer(lineString, regex = /[^a-zA-Z0-9\-,\.]/g) {
  */
 function reformatDate(dateString) {
   try {
-    if (typeof Date.parse(dateString) === "number") {
+    const regex = /([12]\d{3,3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+    const match = dateString.match(regex);
+    if (match) {
       const dateElements = dateString.split("-").reverse();
       return dateElements.join("/").toString();
+    } else {
+      throw new Error(
+        `${dateString} not a correct data format. check metadata maps with data`
+      );
     }
   } catch (ex) {
-    console.log(new Error(`1 File's format isn't correct`));
+    console.log(
+      new Error(
+        `${dateString} not a correct data format. check metadata maps with data`
+      )
+    );
+    process.exit(1);
   }
 }
 
@@ -54,18 +68,28 @@ function reformatDate(dateString) {
  */
 function checkDataType(value, dataType) {
   dataType = stringSanitizer(dataType, /\s\n\r/);
-  if (dataType === "date") {
-    return reformatDate(value);
-  } else if (dataType === "string") {
-    return value;
-  } else if (dataType === "numeric") {
-    try {
-      return parseFloat(value);
-    } catch (ex) {
-      console.log(new Error("File not in the right format"));
+  try {
+    if (dataType === "date") {
+      return reformatDate(value);
+    } else if (dataType === "string") {
+      return value;
+    } else if (dataType === "numeric") {
+      try {
+        return parseFloat(value);
+      } catch (ex) {
+        console.log(
+          new Error(`${value} is not a number. check metadata maps with data`)
+        );
+        process.exit(1);
+      }
+    } else {
+      throw new Error(
+        `Data file value: ${value} not matching data type: ${dataType}`
+      );
     }
-  } else {
-    console.log(new Error("File not in the right format"));
+  } catch (ex) {
+    console.log(ex.message);
+    process.exit(1);
   }
 }
 
@@ -104,46 +128,69 @@ function createFileHeader(metaArray) {
 function lineFormater(line, metaArray) {
   let newLine = "";
   let substringFromChar = 0;
-  const metaLenght = metaArray.length;
-  for (let i = 0; i < metaLenght; i++) {
-    const substringToChar = parseInt(extractColumnsMetaData(metaArray[i])[1]);
-    const dataType = extractColumnsMetaData(metaArray[i])[2];
-    let subString = line.substring(
-      substringFromChar,
-      substringFromChar + substringToChar
-    );
-    subString = checkDataType(subString, dataType.trim());
-    if (i < metaLenght - 1) {
-      newLine += subString;
-      newLine += ",";
-    } else {
-      newLine += subString;
+  try {
+    const metaLenght = metaArray.length;
+    for (let i = 0; i < metaLenght; i++) {
+      const substringToChar = parseInt(extractColumnsMetaData(metaArray[i])[1]);
+      const dataType = extractColumnsMetaData(metaArray[i])[2];
+      let subString = line.substring(
+        substringFromChar,
+        substringFromChar + substringToChar
+      );
+      subString = checkDataType(subString, dataType.trim());
+      if (subString.toString().includes(",")) {
+        subString = '"'.concat(subString).concat('"');
+      }
+      if (i < metaLenght - 1) {
+        newLine += subString;
+        newLine += ",";
+      } else {
+        newLine += subString;
+      }
+      substringFromChar = substringFromChar + substringToChar;
     }
-    substringFromChar = substringFromChar + substringToChar;
+    return stringSanitizer(newLine, /\s/g);
+  } catch (ex) {
+    console.log(ex.message);
+    process.exit(1);
   }
-  return stringSanitizer(newLine, /\s/g);
 }
 /**
  * Create the data.csv file
- * @param {String} dataFile
- * @param {String} metaData
+ * @param {String} fixedFileFormat
+ * @param {String} metadataFile
  * @param {String} wherToSave
  */
-function generate_csv(dataFile, metaData, wherToSave) {
-  dataColumns(metaData)
+function generate_csv(fixedFileFormat, metadataFile, wherToSave) {
+  dataColumns(metadataFile)
     .then(data => {
-      const rl = readLine(dataFile);
+      const rl = readLine(fixedFileFormat);
       const metaArray = data.toString().split("\n");
-      writeToFile(wherToSave, createFileHeader(metaArray).concat("\n"));
+      let i = 0;
       rl.on("line", line => {
-        const regex = new RegExp(/[^a-zA-Z0-9\-,\.\s]/g);
-        line = stringSanitizer(line, regex);
-        writeToFile(wherToSave, lineFormater(line, metaArray).concat("\n"));
+        try {
+          const regex = new RegExp(/[^a-zA-Z0-9\-,\.\s]/g);
+          line = stringSanitizer(line, regex);
+          if (i === 0) {
+            writeToFile(wherToSave, createFileHeader(metaArray).concat("\n"));
+            writeToFile(wherToSave, lineFormater(line, metaArray).concat("\n"));
+            i++;
+          } else {
+            writeToFile(wherToSave, lineFormater(line, metaArray).concat("\n"));
+          }
+        } catch (ex) {
+          console.log(ex.message);
+          process.exit(1);
+        }
       });
-      console.log("Working...");
+      rl.on("end", () => {
+        console.log(i);
+      });
+      console.log("Please wait...");
     })
     .catch(err => {
-      console.log(new Error("something went worng"));
+      console.log(new Error(err.message));
+      return;
     });
 }
 /**
@@ -153,11 +200,16 @@ function generate_csv(dataFile, metaData, wherToSave) {
  * @param {String} input
  */
 function writeToFile(path = "datafile.csv", input) {
-  var logger = fs.createWriteStream(path, {
-    flags: "a" // 'a' means appending (old data will be preserved)
-  });
-  logger.write(input);
-  logger.end();
+  try {
+    var logger = fs.createWriteStream(path, {
+      flags: "a"
+    });
+    logger.write(input);
+  } catch (ex) {
+    console.log(new Error("While saving the file something went wrong"));
+  } finally {
+    logger.end();
+  }
 }
 
 module.exports.stringSanitizer = stringSanitizer;
